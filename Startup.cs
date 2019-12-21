164dae598593b9a76bd5b722dad4a3dd2b43459a
MurtajaKadiyani <murtaja.mainframe@gmail.com>
2019-12-21 14:03:06 +0530

    second commit

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace AspNetCoreOnDocker
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Murtaja!!Welcome to Docker World!!");
             });
         }
     }
